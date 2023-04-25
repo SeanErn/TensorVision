f90b2b350c0d047e39dc97b1a1fbf3708861f616
FunkyChicken <81441231+SeanErn@users.noreply.github.com>
2023-04-25 19:40:58 +0000

    add some backend stuffs

---
+++ Backend/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tfForCo_Backend',
+      version='0.1',
+      description='The funniest joke in the world',
+      setup_requires=[
+          'websockets',
+      ]
+)
