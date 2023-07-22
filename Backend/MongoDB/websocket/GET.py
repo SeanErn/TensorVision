872e22ba5c0592c1555afe74e4b27b1679eacf7e
SeanErn <seanernstes@gmail.com>
2023-07-21 23:41:47 -0400

    update python/mongodb scripts

--- Backend/MongoDB/websocket/GET.py
+++ Backend/MongoDB/websocket/GET.py
@@ -8,44 +8,59 @@ from websocket.constants import *
 # cameraSettings
 def getCameraDevice(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.device': 1, '_id': 0})["cameraSettings"]
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.device': 1, '_id': 0})["cameraSettings"]
+    return status.ok_send_data(dataFormatted)
+    
 
 def getCameraExposure(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.exposure': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.exposure': 1, '_id': 0})["cameraSettings"]
+    return status.ok_send_data(dataFormatted)
 
 def getCameraBrightness(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.brightness': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.brightness': 1, '_id': 0})["cameraSettings"]
+    return status.ok_send_data(dataFormatted)
 
 def getCameraAutoExposure(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.autoExposure': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.autoExposure': 1, '_id': 0})["cameraSettings"]
+    return status.ok_send_data(dataFormatted)
 
 def getInputImageRotationMode(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.inputImageRotationMode': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'cameraSettings.inputImageRotationMode': 1, '_id': 0})["cameraSettings"]
+    return status.ok_send_data(dataFormatted)
 
 # -----------------------------------------------------------------------------------------------------------------------------------------
 
 # pipelineSettings
 def getModel(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'model': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'pipelineSettings.model': 1, '_id': 0})["pipelineSettings"]
+    return status.ok_send_data(dataFormatted)
 
 def getMinimumConfidence(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'minimumConfidence': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'pipelineSettings.minimumConfidence': 1, '_id': 0})["pipelineSettings"]
+    return status.ok_send_data(dataFormatted)
 
 # -----------------------------------------------------------------------------------------------------------------------------------------
 
-# targetingOffsets
+# pipelineSettings.targetingOffsets
 def getYaw(data: json):
     pipelineName = data["pipelineName"]
-    return pipelines.find_one({"pipelineName": pipelineName}, {'yaw': 1})
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'pipelineSettings.targetingOffsets.yaw': 1, '_id': 0})["pipelineSettings"]["targetingOffsets"]
+    return status.ok_send_data(dataFormatted)
 
 def getPitch(data: json):
-    pipelineName = data.pipelineName
-    return pipelines.find_one({"pipelineName": pipelineName}, {'pitch': 1})
+    pipelineName = data["pipelineName"]
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'pipelineSettings.targetingOffsets.pitch': 1, '_id': 0})["pipelineSettings"]["targetingOffsets"]
+    return status.ok_send_data(dataFormatted)
+
+def getAll(data: json):
+    pipelineName = data["pipelineName"]
+    dataFormatted = pipelines.find_one({"pipelineName": pipelineName}, {'_id': 0})
+    return status.ok_send_data(dataFormatted)
 
 # -----------------------------------------------------------------------------------------------------------------------------------------
